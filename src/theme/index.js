5650b38adff95e5ff4abced9de0c58e265414c27
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-08-15 18:26:58 +0530

    project bootstrapped

---
+++ src/theme/index.js
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    dark: "#0A1014",
+    grey: "#1B2329",
+    midGrey: "#353F4C",
+    lightGrey: "#7A8C99",
+    white: "#FFFFFF",
+    yellow: "#FF9F1C",
+    red: "#FF4040",
+    green: "#2EC4BC",
+  },
+
+  font: "Roboto",
+  fontWeights: {
+    xs: {
+      size: "16pt",
+      weight: "400",
+    },
+    sm: {
+      size: "20pt",
+      weight: "500",
+    },
+    md: {
+      size: "24pt",
+      weight: "500",
+    },
+    lg: {
+      size: "80pt",
+      weight: "700",
+    },
+  },
+};
